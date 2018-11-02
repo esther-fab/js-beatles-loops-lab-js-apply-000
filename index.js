1159c5d845358be6aecf35e0c83e0f2830b547f7
Esther Fab <esther.y.fab@icloud.com>
2018-11-02 15:43:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // add solution here
 
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instruments) {
     var l = musicians.length;
     const beatlesArray = [];   
@@ -25,13 +26,52 @@ function johnLennonFacts(facts) {
 
 
   function iLoveTheBeatles(n) {
+=======
+
+
+var l = musicians.length;
+var beatlesArray = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  
+  for (var i=0; i<l; i++) {
+    beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  
+  
+}
+
+
+function johnLennonFacts(lennonFacts) {
+ let i =0;
+
+ while (i < lennonFacts.length) {
+   lennonFacts.push(`${facts[i]}!!!`)
+   i++;
+  }
+  return lennonFacts;
+  
+  
+}
+
+
+
+  function iLoveTheBeatles() {
+>>>>>>> 1101594b185dab0dda48d21a47ada0d00f0339a6
   var arrayBeatles = [];  
   
     do {
       arrayBeatles.push("I love the Beatles!");
+<<<<<<< HEAD
       n++;
     }
     while ( n < 15)
+=======
+      var i = 0;
+      i++;
+    }
+    while ( i < 15)
+>>>>>>> 1101594b185dab0dda48d21a47ada0d00f0339a6
     
   return arrayBeatles;
 }
